0191305281fda22c8dd264f63d241b548417ba4c
Olivia Scarfone <olivia.scarfone@gmail.com>
2022-06-09 15:21:06 -0700

    remove wrapping div on coffeeshop component

--- main/src/coffeeShop/CoffeeShop.js
+++ main/src/coffeeShop/CoffeeShop.js
@@ -1,8 +1,8 @@
 
 export default function CoffeeShop() {
     return (
-        <div className="coffeeShop">
-            <img src="https://nomsmag.gumlet.io/wp-content/uploads/2021/09/best-burnaby-cafe-ki-cafe.jpg"/>
-        </div>
+        
+         <img src="https://nomsmag.gumlet.io/wp-content/uploads/2021/09/best-burnaby-cafe-ki-cafe.jpg"/>
+        
     );
 }
