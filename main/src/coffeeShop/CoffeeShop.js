8f6ceaa979bcffd4cc22df0fb616c4fb246cc5f7
Olivia Scarfone <olivia.scarfone@gmail.com>
2022-06-10 18:03:47 -0700

    added some styling

--- main/src/coffeeShop/CoffeeShop.js
+++ main/src/coffeeShop/CoffeeShop.js
@@ -1,8 +1,8 @@
+import "./styles.css";
 
-export default function CoffeeShop() {
+export default function CoffeeShop(props) {
     return (
-        
-         <img src="https://nomsmag.gumlet.io/wp-content/uploads/2021/09/best-burnaby-cafe-ki-cafe.jpg"/>
-        
+        <img src="https://nomsmag.gumlet.io/wp-content/uploads/2021/09/best-burnaby-cafe-ki-cafe.jpg"/>
+         
     );
 }
--- main/src/feed/Feed.js
+++ main/src/feed/Feed.js
@@ -1,13 +1,17 @@
 import Review from "../review/Review";
 import CoffeeShop from "../coffeeShop/CoffeeShop";
 import ListFrame from "../list/ListFrame";
+import "./styles.css"
 
 function Feed() {
+
+  let sampleReview1 = <Review text="This coffee shop was only okay, they didn't have everything bagels." author="Amy" coffeeShop={<CoffeeShop name="Coffee Place"/>}/>;
+  let sampleReview2 = <Review text="This place was the worst. Coffee was $7 and they didn't even have any to-go cups. They poured the coffee directly in my hands to slurp up on my way to work." author="Ben" coffeeShop={<CoffeeShop name="Cup of Brown"/>}/>;
+  let sampleReview3 = <Review text="Coffee was great, vegan wraps were not" author="Lauren" coffeeShop={<CoffeeShop name="Coffeapolloza"/>}/>;
+  var sampleData = [sampleReview1, sampleReview2, sampleReview3]
     return (
       <div className="body">
-        <ListFrame elements={[<Review text="this is a sample review" author="Amy" coffeeShop={<CoffeeShop/>}/>]}/>
-
-            {/* <Review text="this is a sample review" author="Amy" coffeeShop={<CoffeeShop/>}/> */}
+        <ListFrame elements={sampleData}/>
       </div>
     );
   }
